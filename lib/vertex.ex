b3dc88f264606c24ba5c6fbef622ffd209aaaaa4
Sheharyar Naseer <hello@sheharyar.me>
2023-10-25 04:07:42 +0500

    Initial Commit

---
+++ lib/vertex.ex
@@ -0,0 +1,18 @@
+defmodule Vertex do
+  @moduledoc """
+  Documentation for `Vertex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Vertex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/vertex_test.exs
@@ -0,0 +1,8 @@
+defmodule VertexTest do
+  use ExUnit.Case
+  doctest Vertex
+
+  test "greets the world" do
+    assert Vertex.hello() == :world
+  end
+end
